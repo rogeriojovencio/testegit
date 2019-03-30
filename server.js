bf9ecc31c4cc732df710ff0dad40b96c8591e263
rogeriojovencio <38884800+rogeriojovencio@users.noreply.github.com>
2019-03-30 08:19:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.get('/', (req, res)=>{
     res.status(200).send({message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/', (req, res)=>{
+    res.status(200).send({message: 'Oi eu sou a rota olá'});
+})
+
+
 app.listen(3002, ()=>{
     console.log('Api rodando na porta 3001');
 })
